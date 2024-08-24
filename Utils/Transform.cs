2f68b75d94bb43e371d73090c589c7db0728391a
Federico Iza <fedegiza@gmail.com>
2024-08-24 19:33:04 -0300

    Add rotations to the `Transform` class.

--- Utils/Transform.cs
+++ Utils/Transform.cs
@@ -8,12 +8,29 @@ namespace RasterizationRenderer.Utils
     public class Transform
     {
         public float Scale { get; private set; }
+        public List<Vector3> RotationAngles { get; private set; }
         public Vector3 Translation { get; private set; }
+        public List<Quaternion> Rotations { get; set; }
 
-        public Transform(float scale, Vector3 translation)
+        public Transform(float scale, List<Vector3> rotations, Vector3 translation)
         {
             Scale = scale;
+            RotationAngles = rotations;
             Translation = translation;
+            
+            // Transform rotation angles to quaternions.
+            Rotations = new();
+            foreach (Vector3 v  in rotations)
+            {
+                if (v.Length() <= 0.01f)
+                {
+                    Rotations.Add(Quaternion.Identity);
+                }
+                else
+                {
+                    Rotations.Add(Quaternion.CreateFromAxisAngle(Vector3.Normalize(v), (MathF.PI / 180f) * v.Length()));
+                }
+            }
         }
     }
 }
