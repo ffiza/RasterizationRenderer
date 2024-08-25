0994a8df98199361302112b8cf16ab763376b1a3
Federico Iza <fedegiza@gmail.com>
2024-08-25 15:10:10 -0300

    Use comman line arguments to run program.

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@ namespace RasterizationRenderer
 {
     class Program
     {
-        static void Main()
-        {
+        static void Main(string[] args)
+        { 
             Canvas canvas = new(1000, 1000, Color.White);
-            Scene scene = Scene.FromJSON("C:\\Users\\Usuario\\Documents\\Repositories\\RasterizationRenderer\\Scenes\\ThreeCubes.json");
+            Scene scene = Scene.FromJSON("C:\\Users\\Usuario\\Documents\\Repositories\\RasterizationRenderer\\Scenes\\" + args[0] + ".json");
             scene.Render(canvas);
-            canvas.SaveToPNG("image.png");
+            canvas.SaveToPNG("Renders\\" + args[0] + ".png");
         }
     }
 }
