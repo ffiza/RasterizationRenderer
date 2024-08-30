8e242fea637f32bcadba44e792721b64ab7543a4
Federico Iza <fedegiza@gmail.com>
2024-08-30 07:49:25 -0300

    Add the three clipping planes to `Viewport` and implement them in the scene data file.

---
+++ Utils/Plane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasterizationRenderer.Utils
+{
+    public class Plane
+    {
+        public Vector3 Normal { get; private set; }
+        public float Distance { get; private set; }
+
+        public Plane(Vector3 normal, float distance)
+        {
+            Normal = normal;
+            Distance = distance;
+        }
+    }
+}
