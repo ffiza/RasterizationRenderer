78635e14b7709c92911c7b495ad2baf39d41a610
Federico Iza <fedegiza@gmail.com>
2024-09-06 22:12:39 -0300

    Add the fixed world position of the camera.

--- Utils/Viewport.cs
+++ Utils/Viewport.cs
@@ -8,6 +8,7 @@ namespace RasterizationRenderer.Utils
         public float Width { get; private set; }
         public float Height { get; private set; }
         public List<Plane> ClippingPlanes { get; private set; }
+        public Vector3 CameraPosition { get; private set; }
 
         public Viewport(float distanceToCamera, float width, float height, List<Plane> clippingPlanes)
         {
@@ -15,6 +16,7 @@ namespace RasterizationRenderer.Utils
             Width = width;
             Height = height;
             ClippingPlanes = clippingPlanes;
+            CameraPosition = Vector3.Zero;
         }
     }
 }
